e31bf33754465cd4acb284aa76e0c7800a31d32d
Hashara13 <hnethmin13@gmail.com>
2024-08-08 00:17:18 +0530

    single posting added

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#app_name='posts'
+
 urlpatterns = [
     path('', views.post_home, name='post_home'),
+    path('<slug:slug>/', views.post_content, name='post_content'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def post_home(request):
     # retrive all posts from the Post model
     posts=Post.objects.all().order_by("date")
     return render(request, "posts/post_home.html",{'posts':posts})
+
+def post_content(request,slug):
+    post=Post.objects.get(slug=slug)
+    return render(request, "posts/post_content.html",{'post':post})
