3edd1558984138862cac47637d2e72966c24ef76
Hashara13 <hnethmin13@gmail.com>
2024-08-08 23:30:26 +0530

    minor changes

--- TheEyedde/urls.py
+++ TheEyedde/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from posts import views as postViews
 from . import views
 
 urlpatterns = [
@@ -10,7 +11,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('posts/', include('posts.urls')), 
     path('accounts/', include('users.urls')), 
-    path('', views.home, name='home'),
+    path('', postViews.post_home, name='post_home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
